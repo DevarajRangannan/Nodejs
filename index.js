0e0ff1b4e9d43dc2b6ddfacb8a984ffd6a204cc2
Devaraj Rangannan <devarangan2002@gmail.com>
2023-06-15 22:59:17 +0530

    Code changed

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 
 app.get("/", (req, res)=>{
     console.log("hiiii");
-    res.send("XXX");
+    res.send("Hello World");
+})
+
+app.get("/calc", (req, res)=>{
+    console.log(Date() + req.ip + " ==> " + req.headers.host + req.path);
+    try{
+        r = v + 0;
+    }
+    // catch(err){
+    //     res.send("<h1>Error</h1>");
+    // }
+    
 })
 
 app.listen(4000, ()=>{console.log("Server Start");})
