a4cb39fff59d9bc417e0886641fb4f7d5d9dd912
devaraj rangannan <devarangan2002@gmail.com>
2023-08-30 08:39:50 +0530

    Webhook and db features added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient
+
 require('dotenv').config();
 
+
 const app = express();
+
+
 
 app.get("/", (req, res)=>{
     console.log("hiiii");
@@ -36,6 +41,24 @@ app.get("/featur1", (req, res)=>{
     console.log(Date() + req.ip + " ==> " + req.headers.host + req.path);
     res.send("Feature 1")
 
+})
+
+app.get("/webhook", (req, res)=>{
+    res.send("webhook")
+
+})
+
+app.get("/db", (req, res)=>{
+    var url = "mongodb://localhost:27017/MongoDatabase?serverSelectionTimeoutMS=2000";  
+
+    MongoClient.connect(url, function(err, db) {  
+        if (err){
+            res.send("505 Error")
+            console.log("Db Not Connected");
+        };  
+        console.log("Database created!");  
+        db.close();  
+    }); 
 })
 
 
